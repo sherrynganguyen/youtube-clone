59d21b9ccd9228064ad9c41889cc240b0d0671a1
Nga Nguyen <ngasdragon@gmail.com>
2020-03-09 20:30:39 -0700

    create component js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Grid } from '@material-ui/core';
-
+import {SearchBar, VideoList, VideoDetail} from './components';
 import youtube from './api/youtube';
 
 class App extends React.Component {
